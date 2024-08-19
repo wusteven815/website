b8d5a5bbec4f4e536f9ce1e640a7bfb21f039130
Steven Wu <wusteven815@gmail.com>
2024-08-18 20:10:09 -0400

    fix: experience style

--- src/components/Cards/ExperienceCard.tsx
+++ src/components/Cards/ExperienceCard.tsx
@@ -21,19 +21,22 @@ export default function ExperienceCard({
   role,
   times,
 }: ExperienceCardProps) {
-  const { ref, isVisible } = useIntersectionObserver();
+  const { ref: logoRef, isVisible: isLogoVisible } = useIntersectionObserver();
+  const { ref: detailsRef, isVisible: isDetailsVisible } = useIntersectionObserver();
+
+  const isVisible = isLogoVisible || isDetailsVisible;
 
   return (
     <>
       <div className="flex flex-col items-center row-span-2 gridlg:row-span-1">
-        <div className="w-6 h-6 rounded-full border-4 border-sky-500" />
+        <div className="w-6 h-6 rounded-full border-4 border-sky-400" />
         <div className="w-1 flex-1 bg-sky-500 min-h-8" />
       </div>
 
       <div
         className={`flex motion-safe:transition-all motion-safe:duration-500 justify-center mb-2
           ${!isVisible && "motion-safe:opacity-0 motion-safe:translate-x-16"}`}
-        ref={ref}
+        ref={logoRef}
       >
         <div>
           <Image src={logoUrl} alt={company} width={144} height={144} className="rounded-xl" />
@@ -42,7 +45,8 @@ export default function ExperienceCard({
 
       <div
         className={`motion-safe:transition-all motion-safe:duration-500 motion-safe:delay-100
-          ${!isVisible && "motion-safe:opacity-0 motion-safe:translate-x-16"} ${!noMarginBottom && "mb-12"}`}
+          ${!isVisible && "motion-safe:opacity-0 motion-safe:translate-x-16"} ${!noMarginBottom && "mb-16"}`}
+        ref={detailsRef}
       >
         <div className="flex flex-wrap font-bold text-xl mb-1">
           <h2 className="mr-1">{role}</h2>
