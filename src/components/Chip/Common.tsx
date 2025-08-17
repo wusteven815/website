f26fd9c9a1be567a6c0963f719afebf43428451e
Steven Wu <wusteven815@gmail.com>
2025-08-17 17:38:40 -0400

    format: styling

--- src/components/Chip/Common.tsx
+++ src/components/Chip/Common.tsx
@@ -25,19 +25,35 @@ import Chip from "./Chip";
 import { FaJava } from "react-icons/fa6";
 
 export const CppCard = () => (
-  <Chip name="C++" icon={<SiCplusplus className="w-3 h-3" />} className="bg-[#00599C] text-white" />
+  <Chip
+    name="C++"
+    icon={<SiCplusplus className="w-3 h-3" />}
+    className="bg-[#00599C] text-white"
+  />
 );
 
 export const DatadogCard = () => (
-  <Chip name="Datadog" icon={<SiDatadog className="w-3 h-3" />} className="bg-[#632CA6] text-white" />
+  <Chip
+    name="Datadog"
+    icon={<SiDatadog className="w-3 h-3" />}
+    className="bg-[#632CA6] text-white"
+  />
 );
 
 export const DjangoCard = () => (
-  <Chip name="Django" icon={<SiDjango className="w-3 h-3" />} className="bg-[#092E20] text-white" />
+  <Chip
+    name="Django"
+    icon={<SiDjango className="w-3 h-3" />}
+    className="bg-[#092E20] text-white"
+  />
 );
 
 export const FlaskCard = () => (
-  <Chip name="Flask" icon={<SiFlask className="w-3 h-3" />} className="bg-black text-white" />
+  <Chip
+    name="Flask"
+    icon={<SiFlask className="w-3 h-3" />}
+    className="bg-black text-white"
+  />
 );
 
 export const GitHubActionsCard = () => (
@@ -49,7 +65,11 @@ export const GitHubActionsCard = () => (
 );
 
 export const JavaCard = () => (
-  <Chip name="Java" icon={<FaJava className="w-3 h-3" />} className="bg-[#F80000] text-white" />
+  <Chip
+    name="Java"
+    icon={<FaJava className="w-3 h-3" />}
+    className="bg-[#F80000] text-white"
+  />
 );
 
 export const JavaScriptCard = () => (
@@ -61,7 +81,11 @@ export const JavaScriptCard = () => (
 );
 
 export const KerasCard = () => (
-  <Chip name="Keras" icon={<SiKeras className="w-3 h-3" />} className="bg-[#D00000] text-white" />
+  <Chip
+    name="Keras"
+    icon={<SiKeras className="w-3 h-3" />}
+    className="bg-[#D00000] text-white"
+  />
 );
 
 export const MongoDBCard = () => (
@@ -73,15 +97,27 @@ export const MongoDBCard = () => (
 );
 
 export const NextJsCard = () => (
-  <Chip name="Next.js" icon={<SiNextdotjs className="w-3 h-3" />} className="bg-black text-white" />
+  <Chip
+    name="Next.js"
+    icon={<SiNextdotjs className="w-3 h-3" />}
+    className="bg-black text-white"
+  />
 );
 
 export const OpenAICard = () => (
-  <Chip name="OpenAI" icon={<SiOpenai className="w-3 h-3" />} className="bg-[#412991] text-white" />
+  <Chip
+    name="OpenAI"
+    icon={<SiOpenai className="w-3 h-3" />}
+    className="bg-[#412991] text-white"
+  />
 );
 
 export const OpenCvCard = () => (
-  <Chip name="OpenCV" icon={<SiOpencv className="w-3 h-3" />} className="bg-[#5C3EE8] text-white" />
+  <Chip
+    name="OpenCV"
+    icon={<SiOpencv className="w-3 h-3" />}
+    className="bg-[#5C3EE8] text-white"
+  />
 );
 
 export const PlaywrightCard = () => (
@@ -101,11 +137,19 @@ export const PostgreSQLCard = () => (
 );
 
 export const PythonCard = () => (
-  <Chip name="Python" icon={<SiPython className="w-3 h-3" />} className="bg-[#3776AB] text-white" />
+  <Chip
+    name="Python"
+    icon={<SiPython className="w-3 h-3" />}
+    className="bg-[#3776AB] text-white"
+  />
 );
 
 export const ReactCard = () => (
-  <Chip name="React" icon={<SiReact className="w-3 h-3" />} className="text-[#61DAFB] bg-black" />
+  <Chip
+    name="React"
+    icon={<SiReact className="w-3 h-3" />}
+    className="text-[#61DAFB] bg-black"
+  />
 );
 
 export const ReactNativeCard = () => (
@@ -117,11 +161,19 @@ export const ReactNativeCard = () => (
 );
 
 export const RedisCard = () => (
-  <Chip name="Redis" icon={<SiRedis className="w-3 h-3" />} className="bg-[#FF4438] text-white" />
+  <Chip
+    name="Redis"
+    icon={<SiRedis className="w-3 h-3" />}
+    className="bg-[#FF4438] text-white"
+  />
 );
 
 export const ReduxCard = () => (
-  <Chip name="Redux" icon={<SiRedux className="w-3 h-3" />} className="bg-[#764ABC] text-white" />
+  <Chip
+    name="Redux"
+    icon={<SiRedux className="w-3 h-3" />}
+    className="bg-[#764ABC] text-white"
+  />
 );
 
 export const ScikitLearnCard = () => (
